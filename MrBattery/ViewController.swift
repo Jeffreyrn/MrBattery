00d47e0ba688c40cd411defdc1a65637059fd82a
jeffrey <jeffreywangxiao@gmail.com>
2016-05-11 23:35:52 +0800

    label show

--- MrBattery/ViewController.swift
+++ MrBattery/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var CycleLabel: UILabel!
+    @IBOutlet weak var HeathyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UIDevice.currentDevice().batteryMonitoringEnabled = true;
+        // shows the battery level on labels
+        CycleLabel.text = "\(batteryLevel() * 100)%"
+        
+        HeathyLabel.text = "\(batteryState())"
+        
+        print("\(batteryLevel() * 100)%")
+        print("\(batteryState())")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // function to return the devices battery level
+    func batteryLevel()-> Float {
+        return UIDevice.currentDevice().batteryLevel
+    }
+    
+    // function to return the devices battery state (Unknown, Unplugged, Charging, or Full)
+    func batteryState()-> UIDeviceBatteryState {
+        return UIDevice.currentDevice().batteryState
+    }
 
 }
 
